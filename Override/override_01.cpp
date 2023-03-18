77a400f25523fc3071e2f248c0570b9e36cbe1db
Vu-Ngoc-Truong <truong.vungoc@meiko.vn>
2023-03-18 08:09:07 +0700

    add override

--- Override/override_01.cpp
+++ Override/override_01.cpp
@@ -23,6 +23,8 @@ public:
 class Cat : public Animal
 {
 public:
+    // từ khóa override để giúp chương trình sáng sửa và trình biên dịch báo lỗi nếu lớp cha không có hàm virtual,
+    // bỏ nó đi chương trình vẫn hoạt động.
     void makeSound() override
     {
         cout << "The cat meows.\n";
@@ -32,14 +34,26 @@ public:
 int main()
 {
     Animal *animalPtr;
+    Animal animal;
     Dog dog;
     Cat cat;
+
+    animalPtr = &animal;
+    animalPtr->makeSound();
 
     animalPtr = &dog;
     animalPtr->makeSound();
 
     animalPtr = &cat;
     animalPtr->makeSound();
+
+    Dog *dogPtr;
+    dogPtr = &dog;
+    dogPtr->makeSound();
+
+    //! Con trỏ của lớp con không thể trỏ tới lớp cha được
+    // dogPtr = &animal;
+    // dogPtr->makeSound();
 
     return 0;
 }
